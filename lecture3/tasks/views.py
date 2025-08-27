83bd11dd0efa8003a63989a145101c3e7c79cfe2
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-08-27 13:10:58 +0530

    update work

---
+++ lecture3/tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+tasks = ["yash", "ujjwal", "karan"]
+# Create your views here.
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks":tasks
+    })
