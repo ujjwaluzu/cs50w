c6109dfb02b1a7146f35e2d023ecfab9c91340e2
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-08-27 12:44:38 +0530

    update work

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("brain", views.brain, name="brain"),
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "hello/index.html")
+
+def brain(request):
+    return HttpResponse("hello, brain")
+
+def david(request):
+    return HttpResponse("hello, david")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
