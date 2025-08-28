12ae07556f64a87b7794526070d7f90f77ffd9f5
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-08-28 11:15:59 +0530

    update work

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django import forms
 tasks = ["yash", "ujjwal", "karan"]
 # Create your views here.
 
+
+class NewFormTask(forms.Form):
+    task = forms.CharField(label="New Task")
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks":tasks
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html",{
+        "form":NewFormTask()
+    })
