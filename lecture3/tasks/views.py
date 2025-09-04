31c9cddb92278b7f8e18d0d8c7b20058ac25f0f6
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-04 12:09:32 +0530

    working on form

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django import forms
-tasks = ["yash", "ujjwal", "karan"]
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+tasks = []
 # Create your views here.
 
 
@@ -16,7 +18,12 @@ def add(request):
         form = NewFormTask(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            task.append(task)
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
     return render(request, "tasks/add.html",{
         "form":NewFormTask()
     })
