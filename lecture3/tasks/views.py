ff8b9b0d41f393523512558841de72aef9ab83d9
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-04 12:50:41 +0530

    working on form

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -9,8 +9,10 @@ tasks = []
 class NewFormTask(forms.Form):
     task = forms.CharField(label="New Task")
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks":tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -18,7 +20,7 @@ def add(request):
         form = NewFormTask(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
