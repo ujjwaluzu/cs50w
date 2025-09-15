1beb06561aa2e69199f67e7c1758d4123e8448db
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-15 22:45:10 +0530

    passenger model update

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 # Register your models here.
 admin.site.register(Flight)
 admin.site.register(Airport)
+admin.site.register(Passenger)
--- airline/flights/models.py
+++ airline/flights/models.py
@@ -15,7 +15,7 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
-class passenger(models.Model):
+class Passenger(models.Model):
     name = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
     flight = models.ManyToManyField(Flight, blank=True, related_name="passengers")
