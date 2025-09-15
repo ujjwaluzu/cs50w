25b2334a0d790291db8e00c3c25f737614f90bb5
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-15 22:42:48 +0530

    passenger model added

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -14,3 +14,11 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class passenger(models.Model):
+    name = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flight = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
