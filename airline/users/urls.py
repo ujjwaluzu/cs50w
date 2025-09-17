1073560e6e89c4feb5468572ec53cf77a0d9cbbc
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-17 16:56:56 +0530

    working on users

--- airline/users/urls.py
+++ airline/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
-urlpattern = [
-    path("", views.index, name="index")
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
