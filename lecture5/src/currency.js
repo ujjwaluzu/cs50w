fb74ef636160ea4fa8fcb0d9b34182d88fa4be87
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-28 22:06:14 +0530

    lecture 6 started

--- lecture5/src/currency.js
+++ lecture5/src/currency.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
-    fetch("https://api.exchangeratesapi.io/latest?base=USD")
+    document.querySelector('form').onsubmit = function() {
+    // event.preventDefault();
+    fetch("https://open.er-api.com/v6/latest/USD")
+
+    .then(response => response.json())
+    .then(data => {
+        const currency = document.querySelector("#currency").value.toUpperCase();
+        const rate = data.rates[currency]
+
+        if (rate!== undefined) {
+            document.querySelector("h1").innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}`;
+        } else{
+            document.querySelector("h1").innerHTML = "Invalid Currency.";
+        }
+        
+    })
+    .catch(error => {
+        console.error("Error fetching exchange rates:", error);
+    });
+    return false;
+}
 });
+
