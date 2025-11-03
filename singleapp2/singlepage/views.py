db5245c02dbd99965d4b8bb95d93a241ba7d9fa1
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-11-03 16:50:11 +0530

    woeking on singleapp2

--- singleapp2/singlepage/views.py
+++ singleapp2/singlepage/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "singlepage/index.html")
+texts = ["text1", "text2", "text3"]
+def section(request, num):
+    if 1 <= num <= 3:
+        return HttpResponse(texts(num-1))
+    else:
+        raise Http404("No such section")
