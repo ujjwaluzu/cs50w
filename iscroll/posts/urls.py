e15f8495532dd87be228aa31c85366abdc00d8a8
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-11-04 08:18:40 +0530

    Infinite Scroll done

--- iscroll/posts/urls.py
+++ iscroll/posts/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from . import views
 urlpatterns = [
-    
+    path("", views.index, name="index"),
+    path("posts", views.posts, name="posts")
 ]
--- iscroll/posts/views.py
+++ iscroll/posts/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import time
 # Create your views here.
 def index(request):
-    return render (request, "posts/index.html");
+    return render (request, "posts/index.html")
+
+def posts(request):
+
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or 9)
+
+    data = []
+    for i in range(start, (end +1)):
+        data.append(f"post #{i}")
+
+    time.sleep(1)
+
+    return JsonResponse({
+        "posts": data
+    })
