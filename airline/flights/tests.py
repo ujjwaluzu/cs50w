946b433ad30e09c38431decc2806ab47b86e37c4
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2026-01-22 23:25:00 +0530

    tests.py -  defined a setUp function that will be run at the start of the testing process.

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from .models import Flight, Airport,Passenger
 # Create your tests here.
+
+def FlightTestCase(TestCase):
+    def setUp(self):
+
+        # Create airports.
+        a1 = Airport.objects.create(code="AAA", city="City A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
+
+        # Create flights.
+        Flight.objects.create(origin=a1, destination=a2, duration=100)
+        Flight.objects.create(origin=a1, destination=a1, duration=200)
+        Flight.objects.create(origin=a1, destination=a2, duration=-100)
