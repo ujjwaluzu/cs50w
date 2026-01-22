e4ae610a388e6c0529b2158a12351dd6cfd8a48c
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2026-01-22 23:36:58 +0530

    tests.py -  added some more functions

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -20,3 +20,19 @@ def FlightTestCase(TestCase):
     def test_arrivals_count(self):
         a =Airport.objects.get(code="AAA")
         self.assertEqual(a.arrivals.count(), 1)
+
+    def test_valid_flight(self):
+        a1 = Airport.objects.get(code="AAA")
+        a2 = Airport.objects.get(code="BBB")
+        f = Flight.objects.get(origin=a1, destination=a2, duration=100)
+        self.assertTrue(f.is_valid_flight())
+    def test_invalid_flight_destination(self):
+        a1 = Airport.objects.get(code="AAA")
+        f = Flight.objects.get(origin=a1, destination=a1)
+        self.assertFalse(f.is_valid_flight())
+
+    def test_invalid_flight_duration(self):
+        a1 = Airport.objects.get(code="AAA")
+        a2 = Airport.objects.get(code="BBB")
+        f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
+        self.assertFalse(f.is_valid_flight())
